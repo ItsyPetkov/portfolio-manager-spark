c862fb4e958a8926d5f828a1c458c98511fbbeb2
Ross Craig CSM2016 <ross.craig.2016@uni.strath.ac.uk>
2018-02-14 14:09:49 +0000

    Displays all details in tabular format

---
+++ imanidiotcantpush-master-09202c1a49ef5e094e677da89b94c64ef3eca69e/src/main/java/uk/co/pm/controller/EquityController.java
@@ -0,0 +1,67 @@
+package uk.co.pm.controller;
+
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+import uk.co.pm.externalapi.PersonExternalApiService;
+import uk.co.pm.model.Equity;
+import uk.co.pm.model.HelloReference;
+import uk.co.pm.model.Person;
+import uk.co.pm.service.EqService;
+import uk.co.pm.service.HelloMessageService;
+import uk.co.pm.externalapi.*;
+import uk.co.pm.model.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static uk.co.pm.utils.Renderer.render;
+import static uk.co.pm.utils.ResponseTypeUtil.shouldReturnHtml;
+
+public class EquityController {
+
+    private Gson gson;
+    private final EquityExternalApiService EquityExternalApiService;
+
+    public EquityController(String remoteApiBaseUrl) {
+        gson = new Gson();
+        EquityExternalApiService = new EquityExternalApiService(remoteApiBaseUrl);
+        setupRoutes();
+    }
+
+    private void setupRoutes() {
+        //This is how you set up a REST GET method, using Spark (http://sparkjava.com)
+        get("/equities", (Request request, Response response) -> 
+        
+        {
+            //call the external api
+            List<Equity> Equities = EquityExternalApiService.getEquities();
+            
+            //Here, we check whether the request is for html, or whether we should return JSON
+            if (shouldReturnHtml(request)) {
+                //Create a map to represent to "model"
+                Map<String, Object> model = new HashMap<>();
+                model.put("equities", Equities);
+
+                //The path to the tells the program to go look in the resources/templates folder for a file called hello.vm
+                //The template is written in a templating language called Velocity (http://velocity.apache.org/)
+                return render(model, "templates/equity.vm");
+            } else {
+            	
+                //Change the Person objects we got from the external API into the format we want to return to our users
+                List<EquityModel> Eqm = new ArrayList<>();
+                
+                for (Equity eq : Equities) {
+                    Eqm.add(EqService.getStuff(eq));
+                }
+              
+                //Use Gson (Google's json parser) to turn the Java object into json
+              
+           
+                return gson.toJson(Eqm);
+            }
+        });
+    }
+}
---
+++ imanidiotcantpush-master-09202c1a49ef5e094e677da89b94c64ef3eca69e/src/main/java/uk/co/pm/externalapi/EquityExternalApiService.java
@@ -0,0 +1,56 @@
+package uk.co.pm.externalapi;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import uk.co.pm.model.Equity;
+
+public class EquityExternalApiService {
+	
+	 //This is used to communicate with the extenal API (http://square.github.io/okhttp/)
+    private OkHttpClient client;
+    //This is used for converting back & forth between JSON and java objects (https://github.com/google/gson)
+    private Gson gson;
+    private String baseUrl;
+
+    //This is used to turn a json representation of a list of Equity into a List<Equity>
+    //If this was to be a list of another type, it would just be changed to TypeToken<List<MyType>>
+    private TypeToken<List<Equity>> equitiesListTypeToken = new TypeToken<List<Equity>>() {
+    };
+
+    public EquityExternalApiService(String baseUrl) {
+        this.client = new OkHttpClient();
+        this.gson = new Gson();
+        
+        this.baseUrl = baseUrl;
+    }
+
+    public List<Equity> getEquities() throws IOException {
+        String url = baseUrl + "/equities";
+
+        //Create a okHttp "request"
+        Request request = new Request.Builder().url(url).build();
+
+        //Use the okHttp client to make a call, using our request object, returning a response
+        Response response = client.newCall(request).execute();
+
+        //Extract the response body as a string
+        String responseString = response.body().string();
+        System.out.println(responseString);
+        //use Gson to turn your json string into a list of Person objects
+        System.out.println(gson.fieldNamingStrategy());
+        List<Equity> equities = gson.fromJson(responseString, equitiesListTypeToken.getType());
+       
+
+        return equities;
+    }
+	
+}
