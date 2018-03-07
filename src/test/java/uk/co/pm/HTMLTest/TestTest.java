d0f70b0fb4a1ac01f62d54f5dc6b084fd42ad43d
Ross Craig <ross@Rosss-MacBook-Pro-2.local>
2018-03-07 23:10:45 +0000

    Added a test for prices aswell using SELENIUM. SELENIUM. SELENIUM

--- src/test/java/uk/co/pm/HTMLTest/TestTest.java
+++ src/test/java/uk/co/pm/HTMLTest/TestTest.java
@@ -9,7 +9,9 @@ import org.openqa.selenium.safari.SafariDriver;
 import uk.co.pm.controller.Controller;
 import uk.co.pm.controller.PriceController;
 import uk.co.pm.externalapi.EquityExternalApiService;
+import uk.co.pm.externalapi.PriceExternalApiService;
 import uk.co.pm.model.Equity;
+import uk.co.pm.model.Price;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,31 +34,62 @@ public class TestTest {
             WebDriver driver = new SafariDriver();
             driver.get("http://localhost:4567/equities");
 
-            //Find the body
-            WebElement element = driver.findElement(By.id("Body"));
-            WebElement csvElement = driver.findElement(By.id("CSV"));
-
 
             WebElement table = driver.findElement(By.id("table1"));
             List<WebElement> allRows = table.findElements(By.tagName("tr"));
             List<Equity> equities = e.getEquities();
             List<String> EPICS = new ArrayList<String>();
+
             for(WebElement row: allRows){
-                    String r = row.getText().trim();
-                    if(r.contains("EPIC is:")){
-                            String EPIC  = r.substring(9);
-                            EPICS.add(EPIC);
-                    }
+                String r = row.getText().trim();
+                if(r.contains("EPIC is:")){
+                    String EPIC  = r.substring(9);
+                    EPICS.add(EPIC);
+                }
             }
             int i = 0;
             for(Equity eq : equities){
                 assertThat(eq.getEPIC()).isEqualTo(EPICS.get(i));
                 i++;
             }
+            //Find the body
+            WebElement element = driver.findElement(By.id("Body"));
+            WebElement csvElement = driver.findElement(By.id("CSV"));
+
+            driver.navigate().to("http://localhost:4567/equities/prices");
+
+            assertThat(runPrice(driver)).isTrue();
             driver.close();
             driver.quit();
         }
 
+
+        public boolean runPrice(WebDriver d) throws IOException {
+
+
+            PriceExternalApiService p = new PriceExternalApiService("https://portfolio-manager-api.herokuapp.com");
+            WebElement table = d.findElement(By.id("table1"));
+            List<WebElement> allRows = table.findElements(By.tagName("tr"));
+            List<Price> equities = p.getPrices();
+            List<String> EPICS = new ArrayList<String>();
+
+            for(WebElement row: allRows){
+                String r = row.getText().trim();
+                if(r.contains("EPIC is:")){
+                    String EPIC  = r.substring(9);
+                    EPICS.add(EPIC);
+                }
+            }
+            int i = 0;
+            while(i<EPICS.size()){
+                assertThat(equities.get(i).getEPIC()).isEqualTo(EPICS.get(i));
+                System.out.println(equities.get(i).getEPIC() +":"+EPICS.get(i));
+                i++;
+            }
+
+            return true;
+
+        }
     //This will be used when we push to a cloud server
     private static int getAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
