35dd01705b7c2b0de4de3a431ad2b7df6a3fc485
Ross Craig <ross@Rosss-MacBook-Pro-2.local>
2018-03-08 15:14:46 +0000

    More fixes, trying to get CI to run

--- src/test/java/uk/co/pm/HTMLTest/TestTest.java
+++ src/test/java/uk/co/pm/HTMLTest/TestTest.java
@@ -30,12 +30,11 @@ public class TestTest {
 
             String remoteApiBaseUrl = "https://portfolio-manager-api.herokuapp.com";
             EquityExternalApiService e = new EquityExternalApiService(remoteApiBaseUrl);
-            new PriceController(remoteApiBaseUrl);
+            new PriceController(remoteApiBaseUrl); 
             new Controller((remoteApiBaseUrl));
-            System.setProperty("webdriver.chrome.driver", "src/driver");
+            System.setProperty("webdriver.chrome.driver", "src/linuxdriver");
             ChromeOptions options = new ChromeOptions();
-            options.setBinary("/usr/lib/google-chrome");
-            WebDriver driver = new SafariDriver();
+            WebDriver driver = new ChromeDriver(options);
             driver.get("http://localhost:4567/equities");
             driver.manage().window().maximize();
 
