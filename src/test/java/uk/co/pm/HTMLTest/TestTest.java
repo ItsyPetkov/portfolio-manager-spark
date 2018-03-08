d533041f39115998852cb9e3ab61c39699fd5d6b
Ross Craig <ross@Rosss-MacBook-Pro-2.local>
2018-03-08 00:15:07 +0000

    Fixing more stuff...

--- src/test/java/uk/co/pm/HTMLTest/TestTest.java
+++ src/test/java/uk/co/pm/HTMLTest/TestTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import uk.co.pm.controller.Controller;
 import uk.co.pm.controller.PriceController;
@@ -30,7 +31,7 @@ public class TestTest {
             EquityExternalApiService e = new EquityExternalApiService(remoteApiBaseUrl);
             new PriceController(remoteApiBaseUrl);
             new Controller((remoteApiBaseUrl));
-
+            System.setProperty("webdriver.chrome.driver", "src/driver");
             WebDriver driver = new ChromeDriver();
             driver.get("http://localhost:4567/equities");
 
